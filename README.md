0661f43151de72020e509d5e68be94364e81ed39
Viktor Schastnyy <victor92sh@gmail.com>
2021-05-16 00:39:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cryptomath-api-proto
+
+A set of proto files for exchanging messages between CryptoMath API microservices and generating TypeScript types from protobuf schemas.
---
+++ build.sh
@@ -0,0 +1,11 @@
+rm -rf ./proto/build
+
+protoc \
+  --plugin=./node_modules/ts-proto/protoc-gen-ts_proto \
+  --ts_proto_out=./proto \
+  --ts_proto_opt=nestJs=true \
+  ./proto/*.proto
+
+mv ./proto/proto ./proto/build
+
+tsc proto/build/*.ts
