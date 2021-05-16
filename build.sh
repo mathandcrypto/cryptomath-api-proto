e4215d4c41d2672256c7223c0e0fa851387e799d
Viktor Schastnyy <victor92sh@gmail.com>
2021-05-16 21:02:16 +0300

    Added useDate flag to ts-proto generator

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ protoc \
   --plugin=./node_modules/ts-proto/protoc-gen-ts_proto \
   --ts_proto_out=./proto \
   --ts_proto_opt=nestJs=true \
+  --ts_proto_opt=useDate=true \
   ./proto/*.proto
 
 mv ./proto/proto ./proto/build
