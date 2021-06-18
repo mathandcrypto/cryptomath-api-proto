ddedcdc59d51317e78a3f756c8d68aab96999a9a
Viktor Schastnyy <victor92sh@gmail.com>
2021-06-19 01:22:04 +0300

    Added base articles proto scheme

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 rm -rf ./types
 
 protoc \
+  --proto_path=./proto \
   --plugin=./node_modules/ts-proto/protoc-gen-ts_proto \
   --ts_proto_out=./proto \
   --ts_proto_opt=nestJs=true \
   --ts_proto_opt=useDate=true \
   ./proto/*.proto
 
-mv ./proto/proto ./types
+mkdir types
+find ./proto -name '*.ts' -exec mv "{}" ./types \;
 
 tsc types/*.ts
